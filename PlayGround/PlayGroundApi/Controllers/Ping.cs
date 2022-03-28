09b160be758a5596f56ef010b6c6b79577280854
David Ball <david.ball@agriculture.gov.au>
2022-03-28 18:53:22 +1100

    Add api project

---
+++ PlayGround/PlayGroundApi/Controllers/Ping.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PlayGroundApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ping : ControllerBase
+    {
+
+        [HttpGet(Name = "GetPing")]
+        public string Get(string message)
+        { 
+            return message; 
+        }
+    }
+}
